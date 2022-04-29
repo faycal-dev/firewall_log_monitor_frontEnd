68d76c57c85c32659112a497bf449297f7c70140
faycal-dev <m.elmogherbi@esi-sba.dz>
2022-04-29 15:29:11 +0100

    adjusting matrice de flux

--- src/views/dashboard/admin/matriceDeFlux.js
+++ src/views/dashboard/admin/matriceDeFlux.js
@@ -1,9 +1,44 @@
 import React from "react";
-import { Spinner, Card, CardBody, Badge, Button, Input } from "reactstrap";
-import { Search } from "react-feather";
+import {
+  Spinner,
+  Card,
+  CardBody,
+  Badge,
+  Button,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Label,
+  FormGroup,
+} from "reactstrap";
+import { Search, Settings } from "react-feather";
+import Select from "react-select";
 import "../../../assets/scss/pages/dashboard-analytics.scss";
 import Axios from "axios";
 import DataTable from "react-data-table-component";
+
+const GroupByOptions = [
+  {
+    value: "Source",
+    label: "Source user ip group",
+    color: "#00B8D9",
+    isFixed: true,
+  },
+  {
+    value: "Destination",
+    label: "Destination user ip group",
+    color: "#0052CC",
+    isFixed: true,
+  },
+  {
+    value: "Destination_service",
+    label: "Destination service",
+    color: "#5243AA",
+    isFixed: true,
+  },
+];
 
 const COLUMNS = [
   {
@@ -11,18 +46,14 @@ const COLUMNS = [
     selector: "source_client_group",
     sortable: true,
     minWidth: "150px",
-    cell: (row) => (
-      <p className="text-bold-500 mb-0">{row.source_client_group}</p>
-    ),
+    cell: (row) => <p className="text-bold-500 mb-0">{row.Source}</p>,
   },
   {
     name: "Destination user ip group",
     selector: "destination_client_group",
     sortable: true,
     minWidth: "150px",
-    cell: (row) => (
-      <p className="text-bold-500 mb-0">{row.destination_client_group}</p>
-    ),
+    cell: (row) => <p className="text-bold-500 mb-0">{row.Destination}</p>,
   },
   {
     name: "Destination service",
@@ -80,11 +111,22 @@ const COLUMNS = [
 const CustomHeader = (props) => {
   return (
     <div className="d-flex flex-wrap justify-content-between">
-      {/* <div className="add-new">
-        <Button.Ripple color="primary">Add New</Button.Ripple>
-      </div> */}
-      <div style={{width:"40%"}} className="position-relative has-icon-left mb-1">
-        <Input placeholder="Chercher par ip ou action ou service" value={props.value} onChange={(e) => props.handleFilter(e)} />
+      <div className="add-new">
+        <Button.Ripple color="primary" onClick={props.toggleModal}>
+          {" "}
+          <Settings size="15" className="mr-1" />
+          Configuration
+        </Button.Ripple>
+      </div>
+      <div
+        style={{ width: "40%" }}
+        className="position-relative has-icon-left mb-1"
+      >
+        <Input
+          placeholder="Chercher par ip ou action ou service"
+          value={props.value}
+          onChange={(e) => props.handleFilter(e)}
+        />
         <div className="form-control-position">
           <Search size="15" />
         </div>
@@ -98,21 +140,33 @@ const MatriceDeFlux = () => {
   const [filteredData, setFilteredData] = React.useState([]);
   const [value, setValue] = React.useState("");
   const [loading, setLoading] = React.useState(false);
+  const [modal, setModal] = React.useState(false);
+  const [groupeByValue, setGroupeByValue] = React.useState("Source");
+  const [detailedIpDest, setDetailedIpDest] = React.useState(true);
+  const [detailedIpSrc, setDetailedIpSrc] = React.useState(false);
 
   const getMatrice = async () => {
+    setModal(false);
     setLoading(true);
-    const response = await Axios.get(
-      "http://127.0.0.1:8000/dashboard/MatriceDeFlux/aaa"
-    );
-    const parsedResponse = JSON.parse(response.data);
-    setData(parsedResponse);
-    setLoading(false);
+    try {
+      const response = await Axios.get(
+        `http://127.0.0.1:8000/dashboard/MatriceDeFlux/?GroupBy=${groupeByValue}&DetailedDest=${
+          !detailedIpDest ? "yes" : "no"
+        }&DetailedSrc=${!detailedIpSrc ? "yes" : "no"}`
+      );
+      const parsedResponse = JSON.parse(response.data);
+      setData(parsedResponse);
+      setLoading(false);
+    } catch {
+      setLoading(false);
+    }
   };
 
   React.useEffect(() => {
     getMatrice();
   }, []);
 
+  console.log(groupeByValue);
   if (loading) {
     return (
       <div
@@ -127,7 +181,7 @@ const MatriceDeFlux = () => {
       >
         <Spinner type="grow" color="primary" size="lg" />
         <Badge style={{ marginTop: 30, padding: 10 }} color="primary" pill>
-          Téléchargement de 100K logs cela peut prendre quelque secondes
+          Chargement de 100K logs cela peut prendre quelques secondes
         </Badge>
       </div>
     );
@@ -141,7 +195,7 @@ const MatriceDeFlux = () => {
     setValue(value);
 
     if (value.length) {
-        filtered = DATA.filter((item) => {
+      filtered = DATA.filter((item) => {
         let startsWithCondition =
           item.source_client_group
             .toLowerCase()
@@ -173,20 +227,87 @@ const MatriceDeFlux = () => {
     }
   };
 
+  const handleChange = (newValue) => {
+    if (newValue !== null) {
+      setGroupeByValue(newValue.value);
+    } else {
+      setGroupeByValue("Source");
+    }
+  };
+
+  const toggleModal = () => {
+    setModal((prevState) => !prevState);
+  };
+
   return (
     <React.Fragment>
+      <Modal
+        isOpen={modal}
+        toggle={toggleModal}
+        className="modal-dialog-centered"
+      >
+        <ModalHeader toggle={toggleModal}>Configuration</ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Label for="email">Groupe by:</Label>
+            <Select
+              value={groupeByValue}
+              name="groupBy"
+              options={GroupByOptions}
+              className="React"
+              classNamePrefix="select"
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              className="ml-25"
+              type="radio"
+              value={detailedIpDest}
+              onChange={() => {}}
+              onClick={() => {
+                setDetailedIpDest((prevState) => !prevState);
+              }}
+              checked={detailedIpDest}
+            />
+            <Label className="ml-2">Address destination detaillée ?</Label>
+          </FormGroup>
+          <FormGroup>
+            <Input
+              className="ml-25"
+              type="radio"
+              value={detailedIpSrc}
+              onChange={() => {}}
+              onClick={() => {
+                setDetailedIpSrc((prevState) => !prevState);
+              }}
+              checked={detailedIpSrc}
+            />
+            <Label className="ml-2">Address source detaillée ?</Label>
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={getMatrice}>
+            Ok
+          </Button>{" "}
+        </ModalFooter>
+      </Modal>
       <Card>
         <CardBody className="rdt_Wrapper">
           <DataTable
             className="dataTable-custom"
             data={value.length ? filteredData : data}
-            columns={COLUMNS}
+            columns={groupeByValue === "Source" ? COLUMNS : groupeByValue === "Destination" ? [COLUMNS[1],COLUMNS[0],COLUMNS[2],COLUMNS[3],COLUMNS[4]] : [COLUMNS[2],COLUMNS[0],COLUMNS[1],COLUMNS[3],COLUMNS[4]]}
             noHeader
             pagination
             subHeader
             paginationPerPage={50}
             subHeaderComponent={
-              <CustomHeader value={value} handleFilter={handleFilter} />
+              <CustomHeader
+                value={value}
+                handleFilter={handleFilter}
+                toggleModal={toggleModal}
+              />
             }
           />
         </CardBody>
